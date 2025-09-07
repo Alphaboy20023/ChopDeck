43ece19411c148bdf36d886ee2a368650b9bcb77
Alphaboy20023 <timivictor565@gmail.com>
2025-09-07 18:31:47 +0200

    landing page completed

--- Templates/static/js/scripts.js
+++ Templates/static/js/scripts.js
@@ -4,3 +4,15 @@ const btn = document.getElementById("menu-btn");
     btn.addEventListener("click", () => {
       mobileMenu.classList.toggle("hidden");
     });
+
+const chatBtn = document.getElementById('chatBtn');
+    const chatModal = document.getElementById('chatModal');
+    const closeBtn = document.getElementById('closeBtn');
+
+    chatBtn.addEventListener('click', () => {
+      chatModal.classList.toggle('translate-x-full');
+    });
+
+    closeBtn.addEventListener('click', () => {
+      chatModal.classList.add('translate-x-full');
+    });
