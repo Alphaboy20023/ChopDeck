7270f6792a1b4289dff9933c0735d6d8340a3538
Alphaboy20023 <timivictor565@gmail.com>
2025-09-11 09:24:34 +0200

    new features

--- ChopDeck/urls.py
+++ ChopDeck/urls.py
@@ -22,6 +22,6 @@ import cloudinary.uploader
 import cloudinary.api
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('ChopDeck/', include("ChopDeckApp.urls")),
+    # path('admin/', admin.site.urls),
+    path('', include("ChopDeckApp.urls")),
 ]
