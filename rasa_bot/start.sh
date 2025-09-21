2d55337c757bfea3ab4aeb8925682806f93314f0
Alphaboy20023 <timivictor565@gmail.com>
2025-09-21 12:13:27 +0200

    added bash render

--- rasa_bot/start.sh
+++ rasa_bot/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# Start actions server in background
 rasa run actions --port 5055 &
-
-# Start API server on main port
 rasa run --enable-api --cors "*" --port $PORT --debug
 
 # Keep container running
