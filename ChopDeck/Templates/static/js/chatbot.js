e133cc22a2a1603567985b18ab44c023a39aded2
Alphaboy20023 <timivictor565@gmail.com>
2025-09-21 09:22:46 +0200

    rasa endpoint added

--- ChopDeck/Templates/static/js/chatbot.js
+++ ChopDeck/Templates/static/js/chatbot.js
@@ -36,7 +36,7 @@ async function sendMessage() {
 
   try {
     // https://rasa-bot-eurw.onrender.com/webhooks/rest/webhook
-    const response = await fetch("http://localhost:5005/webhooks/rest/webhook", {
+    const response = await fetch("https://rasa-bot-eurw.onrender.com/webhooks/rest/webhook", {
       method: "POST",
       headers: { "Content-Type": "application/json"},
       body: JSON.stringify({sender: "frontend-user", message}),
