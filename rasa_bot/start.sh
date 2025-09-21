bccf1e64c9c7d3501781328c7630b22d183a3312
Alphaboy20023 <timivictor565@gmail.com>
2025-09-21 15:01:33 +0200

    LF line endings

--- rasa_bot/start.sh
+++ rasa_bot/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Make sure this is Unix line endings (LF)
+
 # Start actions server in background
 rasa run actions --port 5055 &
 
-# Start Rasa server in foreground
-exec rasa run --enable-api --cors "*" --port $PORT --debug
+# Start Rasa server with API enabled
+rasa run --enable-api --cors "*" --port $PORT --debug
+
+# Keep container running
+wait
