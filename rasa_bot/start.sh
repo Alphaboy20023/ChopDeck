64504b4d62c29989849ae3603c31ed750855abcc
Alphaboy20023 <timivictor565@gmail.com>
2025-09-21 14:27:13 +0200

    start.sh

--- rasa_bot/start.sh
+++ rasa_bot/start.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-rasa run actions --port 5055 &
-rasa run --enable-api --cors "*" --port $PORT --debug
+    #!/bin/bash
+    rasa run actions --port 5055 &
+    rasa run --enable-api --cors "*" --port $PORT --debug
 
-# Keep container running
-wait
+    # Keep container running
+    wait
