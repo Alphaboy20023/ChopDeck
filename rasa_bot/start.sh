4608476fe17f166e6c7ef598787a413bc5757457
Alphaboy20023 <timivictor565@gmail.com>
2025-09-22 14:01:13 +0200

    rasa bot fix 002

--- rasa_bot/start.sh
+++ rasa_bot/start.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # Make sure this file has Unix LF line endings
 
-# Start actions server in background
-rasa run actions --port $PORT --enable-api &
+# Start actions server in background on port 5055
+rasa run actions --port 5055 --enable-api &
 
 # Start Rasa server using pre-trained model (no retraining, no TensorFlow needed)
-rasa run --enable-api --cors "*" --port $PORT --model models/20250921-121233.tar.gz --debug
+rasa run \
+  --enable-api \
+  --cors "*" \
+  --port $PORT \
+  --interface 0.0.0.0 \
+  --model models/20250922-135732-brass-music.tar.gz \
+  --debug
 
 # Keep container alive
 wait
