f8da66a372a0b3eddcad649ca1aabbd4e2a201f9
Alphaboy20023 <timivictor565@gmail.com>
2025-09-22 21:04:38 +0200

    added procfile for railway II

--- Procfile
+++
@@ -1,1 +0,0 @@
-worker: cd rasa_bot && rasa run actions --port 5055
--- rasa_bot/start_actions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "=== Starting Rasa Action Server on port 5055 ==="
-exec rasa run actions --port 5055 --debug
-
-# Background Worker
