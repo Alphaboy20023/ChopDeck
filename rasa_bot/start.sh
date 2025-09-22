deb5d934d184beac112838896df1e1a3f7d668c5
Alphaboy20023 <timivictor565@gmail.com>
2025-09-22 14:58:03 +0200

    ship all

--- rasa_bot/start.sh
+++ rasa_bot/start.sh
@@ -1,12 +1,13 @@
+pip install --no-cache-dir --upgrade rasa==3.6.21 rasa-sdk==3.6.2
+
 #!/bin/bash
 # Make sure this file has Unix LF line endings
 
 # Start actions server in background on port 5055
-rasa run actions --port 5055 --enable-api &
+rasa run actions --port 5055 --debug &
 
 # Start Rasa server using pre-trained model (no retraining, no TensorFlow needed)
 rasa run \
-  --enable-api \
   --cors "*" \
   --port $PORT \
   --interface 0.0.0.0 \
